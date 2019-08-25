43e3f4f39e5f0deafbf9dcb185e7c32eeb222224
liu-yi <mail@imliuyi.com>
2019-08-25 14:06:02 +0800

    update

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,7 +3,10 @@ import iView from 'iview';
 // import 'iview/dist/styles/iview.css';
 import '../../my-theme/dist/iview.css'
 import locale from 'iview/dist/locale/en-US';
-
+// import vueAnimate from '@deveodk/vue-animate'
+// You need a specific loader for CSS files like https://github.com/webpack/css-loader
+// import '@deveodk/vue-animate/dist/@deveodk/vue-animate.css'
+import Ring from './components/Ring.vue'
 
 export default ({
   Vue, // the version of Vue being used in the VuePress app
@@ -13,4 +16,10 @@ export default ({
 }) => {
   // ...apply enhancements to the app
   Vue.use(iView, { locale })
+  Vue.use(vueAnimate)
+  Vue.component('ring', Ring)
+  Vue.component('abstract-hue', abstractHue)
+  Vue.component('abstract-saturation', abstractSaturation)
+  Vue.component('ring-blue', ringBlue)
+  Vue.component('ring-green', ringGreen)
 }
